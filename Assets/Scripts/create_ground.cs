c372fb0e8a0eecc6ec893130d92c21e264f2d9d8
Charles Hill <charlesgdhill@gmail.com>
2016-07-26 12:17:40 -0700

    added FPS counter

---
+++ Assets/Scripts/create_ground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class create_ground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
