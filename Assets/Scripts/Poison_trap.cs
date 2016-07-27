ff6baff37df6f4b5f12067ad7ecc109b9721eabb
Charles Hill <charlesgdhill@gmail.com>
2016-07-26 18:22:15 -0700

    fixed terrible merge

---
+++ Assets/Scripts/Poison_trap.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poison_trap : MonoBehaviour {
+    public float duration;
+    public int drain_factor;
+    public float endTime;
+    public int drainsLeft;
+    public float startTime;
+    GameObject trap;
+    GameObject player;
+    // Use this for initialization
+    void Start () {
+        trap = this.gameObject;
+        duration = 5f;
+        drain_factor = 5;
+        
+	}
+    void Awake()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit casterObj = Caster();
+        float distToPlayer = Vector3.Distance(trap.transform.position, casterObj.point);
+
+        if (distToPlayer < 5f)
+        {
+            if (casterObj.collider.name == "RigidBodyFPSController")
+            {
+                print(startTime - Time.time);
+                if (Time.time - startTime >= 1)
+                {
+                    startTime = Time.time;
+
+                    player = casterObj.collider.gameObject;
+
+                    if (!player.GetComponent<player_properties>().isPoisoned())
+                    {
+                        InvokeRepeating("poison", 0, 1f);
+                        player.GetComponent<player_properties>().poisoned = true;
+
+                    }
+                }
+                
+            }
+        }
+    }    
+
+    void poison()
+    {
+        print(startTime + duration);
+        print(Time.time);
+        if (startTime + duration < Time.time)
+        {
+            player.GetComponent<player_properties>().poisoned = false;
+            CancelInvoke();
+        }
+        player.GetComponent<player_properties>().health -= drain_factor;
+
+    }
+    RaycastHit Caster()
+    {
+        RaycastHit hit;
+          if (Physics.Raycast(trap.transform.position, trap.transform.forward, out hit))
+        {
+            //print(hit.point);
+            //print(hit.collider.name);
+        }
+        return hit;
+    }
+
+}
---
+++ Assets/Scripts/player_properties.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player_properties : MonoBehaviour {
+    public float health;
+    public int mana;
+    public int stamina;
+    GameObject player;
+    public bool poisoned;
+    public Text HealthCounter;
+	// Use this for initialization
+	void Start () {
+        player = this.gameObject;
+        health = 100;
+        mana = 100;
+        stamina = 100;
+        poisoned = false;
+        setHealthText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        setHealthText();
+        //health -= .1f;
+        if (health <= 0)
+        {
+            Destroy(player); 
+        }
+	}
+    public bool isPoisoned()
+    {
+        return poisoned;
+    }
+
+    void setHealthText()
+    {
+        HealthCounter.text = "Health: " + health.ToString();
+    }
+}
